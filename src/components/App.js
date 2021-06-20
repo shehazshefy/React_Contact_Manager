8daa4d237fd2f599f032d9f880a0b706cce197ea
Shehaz Shefy <shehazshefy97@gmail.com>
2021-06-20 20:49:24 +0530

    4. Saves data to locaStorage, so that refresh doesnt clear data

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,26 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import Header from './Header';
 import AddContact from './AddContact';
 import ContactList from './ContactList';
 
 function App() {
+  const LOCAL_STORAGE_KEY = "contactsKey";
   const [contacts, setContacts] = useState([]);
 
   const addContactHandler = (contact) => {
     console.log(contact)
     setContacts([...contacts, contact]);
   }
+
+  useEffect( () => {
+    const retrivedContacts = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
+    if(retrivedContacts) setContacts(retrivedContacts);
+  }, []);
+
+  useEffect( () => {
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(contacts));
+  }, [contacts]); //Save the data to local storage.
 
   return (
     <div>
